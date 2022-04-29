6ae2c2b82fe970588c906cfaaef1fef63ae4942d
Guillaume Ayoub <guillaume@courtbouillon.org>
2022-04-29 08:57:42 +0200

    Use GTK version 4

--- radio.py
+++ radio.py
@@ -11,11 +11,15 @@ Radio
 
 import configparser
 import os
-from gi.repository import Gtk, GdkPixbuf, Gst
+
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Gst', '1.0')
+from gi.repository import Gtk, GdkPixbuf, Gst  # noqa
 
 
 CONFIG_FOLDER = os.path.expanduser('~/.config/radio')
-CONFIG = configparser.SafeConfigParser()
+CONFIG = configparser.ConfigParser()
 CONFIG.read(os.path.join(CONFIG_FOLDER, 'config'))
 
 
@@ -26,14 +30,19 @@ class Window(Gtk.ApplicationWindow):
         self.set_icon_name('audio-volume-high')
 
         self.header = Gtk.HeaderBar()
-        self.header.set_title('Radio')
-        self.header.set_show_close_button(True)
+        box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
+        box.set_valign(Gtk.Align.CENTER)
+        title = Gtk.Label.new('Radio')
+        box.append(title)
+        self.subtitle = Gtk.Label.new('')
+        self.subtitle.hide()
+        self.subtitle.set_opacity(0.5)
+        box.append(self.subtitle)
+        self.header.set_title_widget(box)
         self.set_titlebar(self.header)
 
-        self.stop_button = Gtk.Button()
-        self.stop_button.add(
-            Gtk.Image.new_from_icon_name('media-playback-stop-symbolic', 1))
-        self.header.add(self.stop_button)
+        self.stop_button = Gtk.Button.new_from_icon_name('media-playback-stop')
+        self.header.pack_start(self.stop_button)
         self.stop_button.set_sensitive(False)
         self.stop_button.connect('clicked', self.stop)
 
@@ -48,11 +57,9 @@ class Window(Gtk.ApplicationWindow):
         self.icon_view.set_activate_on_single_click(True)
         self.icon_view.connect('item-activated', self.play)
 
-        scroll = Gtk.ScrolledWindow()
-        scroll.add(self.icon_view)
-        self.add(scroll)
+        self.set_child(self.icon_view)
 
-        Gst.init()
+        Gst.init([])
         self.playbin = Gst.ElementFactory.make('playbin', 'player')
 
         for section in CONFIG.sections():
@@ -71,20 +78,24 @@ class Window(Gtk.ApplicationWindow):
         self.playbin.set_state(Gst.State.NULL)
         self.playbin.set_property('uri', self.model[path][1])
         self.playbin.set_state(Gst.State.PLAYING)
-        self.header.set_subtitle(self.model[path][0])
+        self.subtitle.set_text(self.model[path][0])
+        self.subtitle.show()
         self.stop_button.set_sensitive(True)
 
     def stop(self, button):
         self.playbin.set_state(Gst.State.NULL)
-        self.header.set_subtitle(None)
+        self.subtitle.hide()
         self.stop_button.set_sensitive(False)
 
 
 class Radio(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self, application_id='fr.yabz.radio')
+
     def do_activate(self):
         self.window = Window(self)
         self.window.maximize()
-        self.window.show_all()
+        self.window.show()
 
 
 Radio().run()
